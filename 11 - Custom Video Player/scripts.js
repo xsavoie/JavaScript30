8bb45045a28e663fce08d12dd9ec2dfd79daecc5
Xavier Savoie <xaviersavoie@gmail.com>
2022-01-12 12:28:55 -0500

    fixed issue

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -60,4 +60,5 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+
 fullScreen.addEventListener('click', toggleFullScreen); 
